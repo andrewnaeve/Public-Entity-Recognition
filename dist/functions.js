58c883eb9209ca50bcf90567c3fd5f64626a0d86
andrewnaeve <andrewnaeve@gmail.com>
2017-02-26 20:06:20 -0800

    clean repo

--- dist/functions.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var longestPhrase = require("./algorithms").longestPhrase;
-
-exports.per = function (string) {
-
-	var _Symbol = longestPhrase(string.trim().replace(/[.,\/#!@$%\^&\*;:{}=\-_`~()]/g, "").replace(/\s{2,}/g, " "));
-	return _Symbol[0].Symbol;
-};
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var per = require("./functions").per;
-
-var mainExport = {
-	per: per()
-};
-
-module.exports = mainExport;
