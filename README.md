94d97901e59c7e2f0dd097ea3807d12feea2ca76
andrewnaeve <andrewnaeve@gmail.com>
2017-02-26 23:39:40 -0800

    passing tests

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 [![travis build](https://img.shields.io/travis/andrewnaeve/Public-Entity-Recognition.svg)](https://travis-ci.org/andrewnaeve/Public-Entity-Recognition)
-# Public-Entity-Recognition
+## Public-Entity-Recognition
 
 This package scans for the longest consecutive string that matches the description of a publicly traded company, and returns the ticker symbol for the most relevant match. 
 This might be useful for twitter mining, or fintech applications.
@@ -12,9 +12,9 @@ npm install public-entity-recognition
 
 ## Usage
 ```javascript
-const entity = require('public-entity-recognition');
-let scan = entity.per('Toyota Motor said will build a new plant in Baja, Mexico, to build Corolla cars for U.S. NO WAY! Build plant in U.S. or pay big border tax.');
-returns 'TM'
+var entity = require('public-entity-recognition');
+var scan = entity.per('Toyota Motor said will build a new plant in Baja, Mexico, to build Corolla cars for U.S. NO WAY! Build plant in U.S. or pay big border tax.');
+returns { match: 'Toyota Motor', symbol: 'TM', description: 'Toyota Motor Corp Ltd Ord', relevance: 0.4 }
 ```
 ## Remaining Work
-In the future, I plan to modify the repo to return the full title and description of the company with the best match, as well as an array containing lesser matches. 
+In the future, I plan to modify the repo to return a full list of matches in order of relevance. 
--- test/symbol_search.spec.js
+++ test/symbol_search.spec.js
@@ -5,35 +5,35 @@ const per = require('../src/index').per;
 
 // se tests should find ticker symbols
 describe('search algorithm', function () {
-  let traded = per("General Motors is sending Mexican made model of Chevy Cruze to U.S. car dealers-tax free across border. Make in U.S.A.or pay big border tax!");
+  let traded = per("General Motors is sending Mexican made model of Chevy Cruze to U.S. car dealers-tax free across border. Make in U.S.A.or pay big border tax!").symbol;
   it('should find  correct stock symbol', function() {
     expect(traded).to.deep.equal('GM');
   });
 });
 
 describe('search algorithm', function () {
-  let traded = per("Rexnord of Indiana is moving to Mexico and rar viciously firing all of its 300 workers. This is happening all over our country. No more!");
+  let traded = per("Rexnord of Indiana is moving to Mexico and rar viciously firing all of its 300 workers. This is happening all over our country. No more!").symbol;
   it('should find  correct stock symbol', function() {
     expect(traded).to.deep.equal('RXN');
   });
 });
 
 describe('search algorithm', function () {
-  let traded = per("Boeing is building a brand new 747 Air Force One for future presidents, but costs are out of control, more than $4 billion. Cancel order!");
+  let traded = per("Boeing is building a brand new 747 Air Force One for future presidents, but costs are out of control, more than $4 billion. Cancel order!").symbol;
   it('should find  correct stock symbol', function() {
     expect(traded).to.deep.equal('BA');
   });
 });
 
 describe('search algorithm', function () {
-  let traded = per("Toyota Motor said will build a new plant in Baja, Mexico, to build Corolla cars for U.S. NO WAY! Build plant in U.S. or pay big border tax.");
+  let traded = per("Toyota Motor said will build a new plant in Baja, Mexico, to build Corolla cars for U.S. NO WAY! Build plant in U.S. or pay big border tax.").symbol;
   it('should find  correct stock symbol', function() {
     expect(traded).to.deep.equal('TM');
   });
 });
 
 describe('search algorithm', function () {
-  let traded = per("Boycott all Apple products until such time as Apple gives cellphone info to authorities regarding radical Islamic terrorist couple from Cal.");
+  let traded = per("Boycott all Apple products until such time as Apple gives cellphone info to authorities regarding radical Islamic terrorist couple from Cal.").symbol;
   it('should find  correct stock symbol', function() {
     expect(traded).to.deep.equal('AAPL');
   });
@@ -42,31 +42,27 @@ describe('search algorithm', function () {
 // se should not find ticker symbols
 
 describe('search algorithm', function () {
-  let traded = per("company");
+  let traded = per("company").symbol;
   it('should not find a ticker symbol', function() {
     expect(traded).to.deep.equal('');
   });
 });
 
 describe('search algorithm', function () {
-  let traded = per("Getting ready to leave for Melbourne, Florida. See you all soon!");
+  let traded = per("Getting ready to leave for Melbourne, Florida. See you all soon!").symbol;
   it('should not find a ticker symbol', function() {
     expect(traded).to.deep.equal('');
   });
 });
 describe('search algorithm', function () {
-  let traded = per("Don't believe  main stream (fake news) media. White House is running VERY WELL. I inherited a MESS and am in  process of fixing it.");
+  let traded = per("Don't believe  main stream (fake news) media. White House is running VERY WELL. I inherited a MESS and am in  process of fixing it.").symbol;
   it('should not find a ticker symbol', function() {
     expect(traded).to.deep.equal('');
   });
 });
 describe('search algorithm', function () {
-  let traded = per("One of  most effective press conferences I've ever seen! says Rush Limbaugh. Many agree.Yet FAKE MEDIA  calls it differently! Dishonest");
+  let traded = per("One of  most effective press conferences I've ever seen! says Rush Limbaugh. Many agree.Yet FAKE MEDIA  calls it differently! Dishonest").symbol;
   it('should not find a ticker symbol', function() {
     expect(traded).to.deep.equal('');
   });
 });
-
-
-
-
